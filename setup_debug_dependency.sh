be8bc500d3b8ba1ed86edce65901106b1f831a6c
Jingwen Chen <Jingwen.Chen2@amd.com>
2021-06-23 11:18:05 +0800

    Add rg and neovim install and deployment
    Signed-off-by: Jingwen Chen <Jingwen.Chen2@amd.com>

--- setup_debug_dependency.sh
+++ setup_debug_dependency.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+
+repo_path=$(pwd)
+
 #install zsh
-sudo apt install zsh
+sudo apt install zsh -y
 
 #install ohmyzsh
+cd ~
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cp ./ys.zsh-theme ~/.oh-my-zsh/themes
+cp $repo_path/ys.zsh-theme ~/.oh-my-zsh/themes
 #install zsh plugins
 cd ~/.oh-my-zsh/plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
 
 #install fzf
+cd ~
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
@@ -24,6 +29,25 @@ tar xvf linux*.tar.gz
 sudo cp $(pwd)/linux-amd64*/ccat /usr/bin
 
 #install sshpass
-sudo apt install sshpass
+sudo apt install sshpass -y
 
+#install rg
+sudo apt-get install ripgrep -y
 
+#install neovim
+sudo apt-get install python-dev python-pip python3-dev python3-pip -y
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim -y
+
+#deploy neovim
+cp -rf $repo_path/nvim/ ~/.config
+nvim -c PlugInstall
+nvim -c CocInstall coc-marketplace
+nvim -c CocInstall coc-git
+nvim -c CocInstall coc-format-json
+nvim -c CocInstall coc-clangd
+nvim -c CocInstall coc-xml
+nvim -c CocInstall coc-pyright
+nvim -c CocInstall coc-json
+nvim -c CocInstall coc-vimlsp
